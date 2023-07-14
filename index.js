7a2bb762f7ed9c5f6fec24e3cc1b7db862d55fa2
Ramgopal Paneri <rampaneri111@gmail.com>
2023-07-14 22:51:51 +0530

    updated style

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ Array.from(row).forEach(element => {
     element.classList.add('mb-2');
     element.classList.add('mt-2');
 });
+ScrollReveal().reveal('.reveal-0',{ delay: 200 });
+ScrollReveal().reveal('.reveal',{ delay: 300 });
+ScrollReveal().reveal('.reveal-2',{ delay: 500 });
+ScrollReveal().reveal('.reveal-3',{ delay: 600 });
+ScrollReveal({ reset: true });
+
+
